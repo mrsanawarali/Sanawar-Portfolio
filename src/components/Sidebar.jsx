ccb8d21a3d9b3198103837ae22734987e8afa5e6
Sanawar Ali <mrsanawarali@users.noreply.github.com>
2026-05-03 11:48:28 -0700

    Fix sidebar icons to match home and about page - use FaCode and SiFlutter components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { navItems } from "../data";
 import profileImage from "../assets/images/profile2.png";
+import { FaCode } from "react-icons/fa6";
+import { SiFlutter } from "react-icons/si";
 
 export default function Sidebar({ activeSection, onNavigate, collapsed, onToggleCollapse }) {
   return (
@@ -32,7 +34,7 @@ export default function Sidebar({ activeSection, onNavigate, collapsed, onToggle
             <div className="orbit-rotator-sm">
               <div className="orbit-badge-sm orbit-badge-sm-web pointer-events-auto">
                 <span className="orbit-badge-icon-sm" aria-hidden="true">
-                  <svg className="h-3 w-3 text-[#009FA0]" fill="currentColor" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/></svg>
+                  <FaCode className="h-3 w-3 text-[#009FA0]" />
                 </span>
                 <span className="orbit-tooltip-sm">Frontend Developer</span>
               </div>
@@ -40,7 +42,7 @@ export default function Sidebar({ activeSection, onNavigate, collapsed, onToggle
             <div className="orbit-rotator-sm orbit-rotator-sm-alt">
               <div className="orbit-badge-sm orbit-badge-sm-flutter pointer-events-auto">
                 <span className="orbit-badge-icon-sm" aria-hidden="true">
-                  <svg className="h-3 w-3 text-[#54C5F8]" fill="currentColor" viewBox="0 0 24 24"><path d="M15 1H3c-1.1 0-2 .9-2 2v13h2V3h12V1zm4 4H7c-1.1 0-2 .9-2 2v13c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm-1 15H8V7h10v13z"/></svg>
+                  <SiFlutter className="h-3 w-3 text-[#54C5F8]" />
                 </span>
                 <span className="orbit-tooltip-sm orbit-tooltip-sm-alt">Android App Developer</span>
               </div>
